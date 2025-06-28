0bba0059869481bbb70d10af84a05d2ed6e450e7
Nathaly Michel Berroa Fermín <112338823+nmbf02@users.noreply.github.com>
2025-06-28 02:54:30 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'EcoAds RD',
+  description: 'Publicidad que limpia, no contamina',
+  generator: 'nmbf02',
 }
 
 export default function RootLayout({
